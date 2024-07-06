366c41c7d26ee7cc859b7edd18e5a00a8b99b4a7
Danny Cao <dannycao1997@yahoo.com>
2024-07-06 11:34:02 -0400

    debug issue with axios

--- movie-gold-v1/src/App.js
+++ movie-gold-v1/src/App.js
@@ -14,6 +14,9 @@ function App() {
     try
     {
       const response = await api.get("/api/v1/movies");
+
+      console.log(response.data);
+
       setMovies(response.data);
     }
     catch(error)
@@ -30,15 +33,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home movies={movies} />} ></Route>
-          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-          <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
-          <Route path="*" element = {<NotFound/>}></Route>
-        </Route>
-      </Routes>
+
     </div>
   );
 }
